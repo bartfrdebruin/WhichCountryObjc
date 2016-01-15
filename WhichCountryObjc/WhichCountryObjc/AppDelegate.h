02b52b25d3c9ffffac604258665031b3537b43cb
Bart de Bruin <bart.fr.debruin@gmail.com>
2016-01-15 17:13:12 +0100

    First commit of project WhichCountryObjc

---
+++ WhichCountryObjc/WhichCountryObjc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhichCountryObjc
+//
+//  Created by Bart de Bruin on 15-01-16.
+//  Copyright © 2016 BartdeBruin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhichCountryObjc/WhichCountryObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhichCountryObjc
+//
+//  Created by Bart de Bruin on 15-01-16.
+//  Copyright © 2016 BartdeBruin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
